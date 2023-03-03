8b66b23a212f4b1e38b2f149724457cbb44f8b92
helios2003 <ankitdash2019@gmail.com>
2023-03-04 00:00:26 +0530

    Updated crawler file

--- andromeda/crawler.py
+++ andromeda/crawler.py
@@ -9,7 +9,6 @@ import click
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-
 from _parser import Parser
 
 
