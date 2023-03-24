91fb1e5df924d13f51e9ac8d1dd484ae1fc01580
Mehul <mehultodi116@gmail.com>
2023-03-24 22:48:59 +0530

    Decouple crawler and parser

--- andromeda/src/andromeda/crawler.py
+++ andromeda/src/andromeda/crawler.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python
 
-from queue import Queue
 import logging
-import pickle
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
-from andromeda.parser import Parser
-from andromeda.config import LOG_FILE, PROGRESS_FILE
+from andromeda.config import LOG_FILE
 
 
 logger = logging.getLogger(__name__)
@@ -24,11 +21,7 @@ logging.basicConfig(
 CHROMEDRIVER_PATH = '/usr/local/bin/chromedriver'
 
 class Crawler:
-    def __init__(self, crawler_id, chromedriver_path=CHROMEDRIVER_PATH):
-        self.crawler_id = crawler_id
-
-        self.parser = Parser()
-
+    def __init__(self, chromedriver_path=CHROMEDRIVER_PATH):
         options = Options()
         options.add_argument('--no-sandbox')
         options.add_argument("--headless")
@@ -41,19 +34,17 @@ class Crawler:
         page = self.driver.page_source
         return page
 
-    def run(self, link_queue):
+    def run(self, link_queue, data_queue):
         while True:
             try:
+                logging.info("Waiting for a URL")
                 get_link = link_queue.get()
-                logging.info("The global link_queue has %i url(s)", link_queue.qsize())
                 logging.info("Getting %s", get_link)
 
                 page = self.get(get_link)
                 logging.info("Downloaded %s", get_link)
 
-                new_links, _ = self.parser.parse(get_link, page)
-                for new_link in new_links:
-                    link_queue.put(new_link)
+                data_queue.put((get_link, page))
             except Exception as error:
                 logging.error("Failed to get %s: %s", get_link, str(error).split('\n', maxsplit=1)[0])
                 logging.debug(error)
