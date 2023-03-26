4f11cd20f89fad35c3c9205f4e185c2dd482b9f2
Mehul <mehultodi116@gmail.com>
2023-03-26 22:10:11 +0530

    Restructure Flask API

---
+++ api/api/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+
+from andromeda.ranker import BM25
+
+from api.utils import get_metadata
+
+
+def run(test_config=None):
+    app = Flask(__name__)
+
+    @app.route('/', methods=['GET'])
+    def index():
+        return "Welcome to Andromeda's API"
+
+    @app.route('/search', methods=['GET'])
+    def search():
+        query = request.args['query']
+
+        ranker = BM25()
+        results = ranker.get_docs(query)[:5]
+
+        results = [get_metadata(url) for url in results]
+
+        return results
+
+    return app
