4ca32fa9ed75154745771429e8fd40340f6fa5f1
Mehul <mehultodi116@gmail.com>
2023-03-27 01:29:36 +0530

    Remove FREQUENCY_THRESHOLD

--- andromeda/src/andromeda/config.py
+++ andromeda/src/andromeda/config.py
@@ -1,7 +1,7 @@
 import os
 
 LOG_FILE = os.path.join(os.path.dirname(__file__), 'andromeda')
-FREQUENCY_THRESHOLD = 3
+FREQUENCY_THRESHOLD = 0
 DOWNLOAD_DIRECTORY = 'downloads'
 MAXIMUM_RETRIES = 3
 DATABASE = 'prod'
