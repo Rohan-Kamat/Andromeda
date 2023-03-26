5667032aab6b5e252c374e89942525f5bda62707
Mehul <mehultodi116@gmail.com>
2023-03-27 00:15:24 +0530

    Edit prod specs

--- andromeda/start.sh
+++ andromeda/start.sh
@@ -2,4 +2,4 @@ source ../venv/bin/activate
 
 python src/andromeda/setup.py
 
-python src/andromeda/main.py start --n_crawler=10 --n_parser=7 --load
+python src/andromeda/main.py start --n_crawler=10 --n_parser=10 --load
